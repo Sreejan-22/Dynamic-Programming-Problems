f05b228791f971aac68d74f320d02a0381735c9f
Sreejan Chaudhury <64251207+Sreejan-22@users.noreply.github.com>
2021-01-14 13:26:44 +0530

    Create UsingRecursion.cpp

---
+++ Fibonacci/UsingRecursion.cpp
@@ -0,0 +1,13 @@
+int fibonacci(int n) {
+	if(n < 2) {
+		return n;
+	}
+	return fibonacci(n-1) + fibonacci(n-2);
+}
+
+int main() {
+    cout<<fibonacci(1)<<endl;
+    cout<<fibonacci(2)<<endl;
+    cout<<fibonacci(5)<<endl;
+    cout<<fibonacci(8)<<endl;
+}
